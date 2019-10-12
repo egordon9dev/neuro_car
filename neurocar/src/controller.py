0b7a416174bf465ae37fcae40d815c2d99078130
Ethan Gordon <egordon9dev@gmail.com>
2019-10-12 01:13:21 -0400

    added track worlds from RallyCar repo, converted cv_image to a numpy array

--- neurocar/src/controller.py
+++ neurocar/src/controller.py
@@ -1,20 +1,37 @@
 #!/usr/bin/env python
 import rospy
 from geometry_msgs.msg import Twist
-
+from sensor_msgs.msg import Image
+import cv2
+from cv_bridge import CvBridge, CvBridgeError
 from math import sin
+import numpy as np
+
+bridge = CvBridge()
+
+def callback(img):
+    try:
+        cv_image = bridge.imgmsg_to_cv2(img, "bgr8")
+    except CvBridgeError as e:
+        print(e)
+
+    ndarray = np.asarray(cv_image)
+
+    cv2.imshow("Image window", cv_image)
+    cv2.waitKey(3)
 
 def main():
+    sub = rospy.Subscriber('neurocar/camera/image_raw', Image, callback)
     pub = rospy.Publisher('neurocar/cmd_vel', Twist, queue_size=10)
-    rospy.init_node('circler', anonymous=True)
+    rospy.init_node('controller', anonymous=True)
 
     rate = rospy.Rate(2) # 10hz
     msg = Twist()
     msg.linear.x = 0
     msg.angular.z = 3
 
+    msg.linear.x = 5
     while not rospy.is_shutdown():
-        msg.linear.x += .2
         pub.publish(msg)
         rate.sleep()
 
