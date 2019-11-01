dd402112dd7e2cd1c0124aeab770d9706797db79
ben <bschuang@gatech.edu>
2019-11-01 15:08:33 -0400

    Added controls to roomba

--- neurocar_gazebo (copy)/neurocar/src/controller.py
+++ neurocar_gazebo (copy)/neurocar/src/controller.py
@@ -6,8 +6,51 @@ import cv2
 from cv_bridge import CvBridge, CvBridgeError
 from math import sin
 import numpy as np
+from getkey import getkey, keys
 
 bridge = CvBridge()
+
+class Attributes:
+    def __init__(self):
+        self.acceleration = 1
+        self.rotation = 1
+        self.speed = 0
+        self.angle = 0
+
+class Movement:
+    def __init__(self, attributes):
+        #rospy.init_node('move_robot_node', anonymous=False)
+        #self.pub_move = rospy.Publisher("/cmd_vel",Twist,queue_size=10)
+        self.move = Twist()
+        self.attr = attributes
+
+    def publish_vel(self):
+        self.pub_move.publish(self.move)
+
+    def moving(self):
+        self.move.linear.x = self.attr.speed
+        self.move.angular.z = self.attr.angle
+
+    def move_forward(self):
+        self.attr.speed += self.attr.acceleration
+        self.moving()
+
+    def move_backward(self):
+        self.attr.speed -= self.attr.acceleration
+        self.moving()
+
+    def rotate_right(self):
+        self.attr.angle += self.attr.rotation
+        self.moving()
+
+    def rotate_left(self):
+        self.attr.angle -= self.attr.rotation
+        self.moving()
+
+    def stop(self):        
+        self.move.linear.x=0
+        self.move.angular.z=0.0
+    
 
 def callback(img):
     try:
@@ -25,14 +68,23 @@ def main():
     pub = rospy.Publisher('neurocar/cmd_vel', Twist, queue_size=10)
     rospy.init_node('controller', anonymous=True)
 
+    attributes = Attributes()
+    mov = Movement(attributes)
     rate = rospy.Rate(2) # 10hz
-    msg = Twist()
-    msg.linear.x = 0
-    msg.angular.z = 3
 
-    msg.linear.x = 5
     while not rospy.is_shutdown():
-        pub.publish(msg)
+        #movement = raw_input('Enter desired movement: ')
+        key = getkey()
+        if key == 'w':
+            mov.move_forward()
+        elif key == 's':
+            mov.move_backward()
+        elif key == 'a':
+            mov.rotate_left()
+        elif key == 'd':
+            mov.rotate_right()
+
+        pub.publish(mov.move)
         rate.sleep()
 
 if __name__ == '__main__':
@@ -40,3 +92,4 @@ if __name__ == '__main__':
         main()
     except rospy.ROSInterruptException:
         pass
+
